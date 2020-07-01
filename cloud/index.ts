fda67bcf5af4dfcfc7be1d8b07fca150a722fd0d
facebadge <facebadge@mobilana.mobi>
2020-07-01 17:41:21 +0300

    fix deployment

--- cloud/index.ts
+++ cloud/index.ts
@@ -4,17 +4,41 @@ import * as lambda from '@aws-cdk/aws-lambda'
 import * as api from '@aws-cdk/aws-apigateway'
 import * as logs from '@aws-cdk/aws-logs'
 import * as pure from 'aws-cdk-pure'
+import * as hoc from 'aws-cdk-pure-hoc'
 import * as path from 'path'
 
 //
 //
 const app = new cdk.App()
-const stack = new cdk.Stack(app, 'example', {
+const config = {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
   }
-})
+}
+const vsn: string = app.node.tryGetContext('vsn') || 'latest'
+const domain: string = app.node.tryGetContext('domain') || 'example.com'
+
+// ----------------------------------------------------------------------------
+//
+// Common Stack
+//
+// ----------------------------------------------------------------------------
+const common = new cdk.Stack(app, 'example-common', { ...config })
+
+const tlsCertificate = pure.join(common,
+  hoc.common.HostedZone(domain).flatMap(
+    zone => hoc.common.Certificate(`*.${domain}`, zone)
+  ),
+).certificateArn
+
+
+// ----------------------------------------------------------------------------
+//
+// Common Stack
+//
+// ----------------------------------------------------------------------------
+const stack = new cdk.Stack(app, 'example', { ...config })
 
 //
 //
@@ -59,21 +83,19 @@ const gocc = (): cdk.BundlingOptions => {
 
 //
 //
-const Gateway = (): api.RestApiProps => ({
-  deploy: true,
-  deployOptions: {
-    stageName: 'api',
-  },
-  endpointTypes: [api.EndpointType.REGIONAL],
-  failOnWarnings: true,  
+const restapi = hoc.gateway.Api({
+  domain,
+  subdomain: vsn,
+  tlsCertificate
 })
+
 
 pure.join(stack,
   pure.use({
-    rest: pure.iaac(api.RestApi)(Gateway),
+    restapi,
     func: pure.wrap(api.LambdaIntegration)(func),
   }).effect(eff => {
-    const seq = eff.rest.root.addResource('news')
+    const seq = eff.restapi.root.addResource('news')
     const els = seq.addResource('{any+}')
     seq.addMethod('ANY', eff.func)
     els.addMethod('ANY', eff.func)
